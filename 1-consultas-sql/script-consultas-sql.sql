02b30e2cc3271b162bcc5f264c138891b2400997
felipejoq <felipejofreq@gmail.com>
2024-06-18 19:17:32 -0400

    consulta sql: 1

---
+++ 1-consultas-sql/script-consultas-sql.sql
@@ -0,0 +1,10 @@
+-- 1. Liste todas las sucursales del minimarket.
+SELECT
+    idSucursal AS id,
+    nombreSucursal AS nombre,
+    direccionSucursal AS direccion,
+    comunaSucursal AS comuna,
+    telefonoSucursal AS telefono
+FROM sucursal;
+
+-- 2. 
